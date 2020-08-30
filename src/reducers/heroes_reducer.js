772e8e14ff241e760a801d56e2d2ac3660bd0117
Can Guler <canmuhammetguler@gmail.com>
2020-08-30 09:14:17 -0500

    reducers combined and seperated

---
+++ src/reducers/heroes_reducer.js
@@ -0,0 +1,18 @@
+import { ADD_CHARACTER } from "../actions/actions";
+import characters_json from "../data/characters.json";
+
+function heroes(state = [], action) {
+  switch (action.type) {
+    case ADD_CHARACTER:
+      let heroes = [...state, createCharacter(action.id)];
+      return heroes;
+    default:
+      return state;
+  }
+}
+
+function createCharacter(id) {
+  let character = characters_json.find((c) => c.id === id);
+  return character;
+}
+export default heroes;
